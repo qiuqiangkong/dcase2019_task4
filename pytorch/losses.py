8f3fd5b721b54ee014ce1b113d66715507699b14
qiuqiangkong <qiuqiangkong@gmail.com>
2019-03-17 12:51:53 +0000

    add annotations

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -10,11 +10,17 @@ def to_tensor(x):
 
 
 def clipwise_binary_crossentropy(output_dict, target_dict):
+    '''Weakly labelled loss. The output and target have shape of: 
+    (batch_size, classes_num)
+    '''
     return F.binary_cross_entropy(
         output_dict['clipwise_output'], target_dict['weak_target'])
 
 
 def framewise_binary_crossentropy(output_dict, target_dict):
+    '''Strongly labelled loss. The output and target have shape of: 
+    (batch_size, frames_num, classes_num)
+    '''
     output = output_dict['framewise_output']
     target = target_dict['strong_target']
     
