f0afb28a41bc58eb085325c64ae1c47267f69790
qiuqiangkong <qiuqiangkong@gmail.com>
2019-03-17 12:52:34 +0000

    update

--- runme.sh
+++ runme.sh
@@ -7,7 +7,7 @@ WORKSPACE='/vol/vssp/msos/qk/workspaces/dcase2019_task4'
 
 # Hyper-parameters
 GPU_ID=1
-MODEL_NAME='Cnn_9layers'
+MODEL_TYPE='Cnn_9layers'
 BATCH_SIZE=32
 
 # Calculate feature
@@ -20,16 +20,16 @@ python utils/features.py calculate_feature_for_all_audio_files --dataset_dir=$DA
 python utils/features.py calculate_scalar --workspace=$WORKSPACE --data_type='train_weak'
 
 # ------ Train and validate on weak labelled data ------
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_weak' --model_name=$MODEL_NAME --loss_type='clipwise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_weak' --model_type=$MODEL_TYPE --loss_type='clipwise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
 
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_weak' --model_name=$MODEL_NAME --loss_type='clipwise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_weak' --model_type=$MODEL_TYPE --loss_type='clipwise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
 
 # ------ Train and validate on synthetic data using clipwise loss ------
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_name=$MODEL_NAME --loss_type='clipwise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_type=$MODEL_TYPE --loss_type='clipwise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
 
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_name=$MODEL_NAME --loss_type='clipwise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_type=$MODEL_TYPE --loss_type='clipwise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
 
 # ------ Train and validate on synthetic data using framewise loss ------
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_name=$MODEL_NAME --loss_type='framewise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py train --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_type=$MODEL_TYPE --loss_type='framewise_binary_crossentropy' --batch_size=$BATCH_SIZE --cuda
 
-CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_name=$MODEL_NAME --loss_type='framewise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
+CUDA_VISIBLE_DEVICES=1 python pytorch/main.py inference_validation --dataset_dir=$DATASET_DIR --workspace=$WORKSPACE --data_type='train_synthetic' --model_type=$MODEL_TYPE --loss_type='framewise_binary_crossentropy' --iteration=5000 --batch_size=$BATCH_SIZE --cuda
